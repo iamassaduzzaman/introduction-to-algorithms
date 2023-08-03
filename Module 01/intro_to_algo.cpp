ce96cf9e663b2e080112f3434acd9e9e6eae5372
iamassaduzzaman <md.assaduzzaman99@gmail.com>
2023-08-03 09:24:12 +0600

    intro to algo

---
+++ Module 01/intro_to_algo.cpp
@@ -0,0 +1,43 @@
+/*
+-----------------------Algorithms--------------------------------
+
+Input -> set of intstruction (algorithm) -> Output
+
+--- Correct Algorithms
+--- Incorrect Algorithms
+
+-----------------------EFFICIENT ALGORITHMS------------------------
+
+we want an algorithm that's
+1. correct
+2. fast
+3. small space
+4. general
+5. simple
+6. clever
+
+----------------------SEARCHING ALGORITHMS-------------------------
+
+--- Linear Search
+--- Binary Search
+
+----------------------ROADMAP--------------------------------------
+
+-> start 
+-> Asymtotic Analysis (tools) 
+-> Searching 
+-> Divide and Conquer (technique) 
+-> Sorting 
+-> DFS, BFS (graphs) 
+-> Graph Algorith 
+-> Tree Algorithms (graph) 
+-> Greedy Algorithm (technique) 
+-> Factorial Knapsack 
+-> Dynamic Programming (technique) 
+-> 0-1 Knapsack 
+-> the future
+
+
+
+
+*/
